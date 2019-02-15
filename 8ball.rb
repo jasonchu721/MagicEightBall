ce41e429ca1a2071feb69390b5d4fffa74a0ef00
jasonchu721 <47549999+jasonchu721@users.noreply.github.com>
2019-02-14 22:43:15 -0700

    Add files via upload

--- 8ball.rb
+++ 8ball.rb
@@ -1,70 +1,76 @@
 require "pry"
 require "colorize"
 
-class MaficEightBall
-  def initialize 
-    @answers_first = [
-      "Yes",
-      "No",
-      "Nope",
-      "Maybe",
-      "Yes, for sure!",
-      "Most likely",
-      "Ask later",
-      "Cannot predict now"
-    ]
-
-    @answers_second = answers_first.clone
-
-    ask question
+class MagicEightBall
+  def initialize
+  @answers_original = [
+    "Yes",
+    "No",
+    "Maybe",
+    "Yeah for sure",
+    "Def not",
+    "Answer is hazy",
+    "Most likely",
+    "Outlook good",
+    "Merp"
+  ]
+  @answers = @answers_original.clone
+  ask_question
   end
 
   def ask_question
-    puts "Ask me a question, any question! (Type quit to exit)"
-    question = gets.strip
-    case question 
-    when "add_answer"
-      add_answer
-      ask_question
-    when "print_answers"
-      view_answers
-      ask_question
-    when "quit"
-      puts "Bye Felicia!!!"
-    when "reset_answers"
-      reset_answers
+  puts "Please ask the magic 8 ball a question, any question!".cyan 
+  puts "Type quit to stop talking to the magic 8 ball".red
+  question = gets.strip
+    case question
+      when "add_answers"
+        add_answers
+        ask_question
+      when "print_answers"
+        view_answers
+        ask_question
+      when "quit"
+        puts "Bye Felcia, see you around!".red
+      when "reset_answers"
+        reset_answers
+      else
+    puts @answers.sample
+        sleep(1)
+        ask_question
+      end
+  end
+
+  def add_answers
+  puts "What answer would you like to add to the magic 8 ball?".green
+  new_answer = gets.strip.downcase
+  @lc_answers = @answers.map(&:downcase)
+    if @lc_answers.include? new_answer
+    puts "That answer has already been input into this game"
     else
-      puts @answer_second.sample
-      sleep(1)
-      ask_question
+    @answers << new_answer
+    print "'#{new_answer}'"
+    puts " has been added to your answer choices.".green
     end
   end
 
-def reset_answers
-  @answers_second = @answers_first.clone
-  puts "The answers have been reset"
-  ask_question
-end
-
-def view_answers
-  @answers.each do |answer|
-    puts "#{answer}"
+  def reset_answers
+    @answers = @answers_original.clone
+    puts "(answers have been reset)".green
+    ask_question
   end
 
-def add_answers
-  puts "What answer would you like to add to the Magic 8 ball?"
-  @new_answer =gets.strip.downcase
-  @lowercase_answers = @answers_second.map(&:downcase)
-  if @lowercase_answers.include? new_answer
-    puts "Sorry, that answer already exsists"
-  else
-    @answers << new_answer
-    print "'#{new_answer}'".red.on_cyan
-    puts " has been added to your answer choices."
+  def view_answers
+    @answers.each do |answer|
+      puts "#{answer}"
+    end
   end
 end
-
-
 
 ball = MagicEightBall.new
 
+
+#################################################
+#def ask_question
+#def view_answers
+#def add_answers
+#def reset_answer
